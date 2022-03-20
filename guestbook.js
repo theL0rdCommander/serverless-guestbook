0d39d2ccf8e6e959680ae4ba54d61bf472b96c48
Felipe García <37160341+theL0rdCommander@users.noreply.github.com>
2022-03-20 17:34:49 -0300

    apikey

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7c3b0304.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
